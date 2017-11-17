34e040c10b8b0caaf3ddcdc842002edc7cc9bcad
lion1ou <lion1ou@163.com>
2017-11-17 18:43:19 +0800

    temp: build

--- dist/getDevice.js
+++ dist/getDevice.js
@@ -2,7 +2,7 @@
 * @Author: lion1ou
 * @Date:   2017-11-16 20:08:25
 * @Last Modified by:   lion1ou
-* @Last Modified time: 2017-11-17 18:18:13
+* @Last Modified time: 2017-11-16 20:08:30
 */
 /**
  *
@@ -17,7 +17,7 @@ function getOS() {
     if (/mac/i.test(appVersion)) return 'MacOSX'
     if (/win/i.test(appVersion)) return 'windows'
     if (/linux/i.test(appVersion)) return 'linux'
-    if (/iphone/i.test(userAgent) || /ipad/i.test(userAgent) || /ipod/i.test(userAgent)) 'iOS'
+    if (/iphone/i.test(userAgent) || /ipad/i.test(userAgent) || /ipod/i.test(userAgent)) 'ios'
     if (/android/i.test(userAgent)) return 'android'
     if (/win/i.test(appVersion) && /phone/i.test(userAgent)) return 'windowsPhone'
 }
