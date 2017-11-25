c3ca11a0b03e04aca6894a8946c987e6df2fee8e
lion1ou <lion1ou@163.com>
2017-11-25 17:21:28 +0800

    fix：修改目录，index.js暴露方法

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 /*
-* @Author: lion1ou
-* @Date:   2017-11-16 20:06:10
-* @Last Modified by:   lion1ou
-* @Last Modified time: 2017-11-16 20:09:57
-*/
-const getDevice = require('./env/getDevice')
+ * @Author: lion1ou
+ * @Date:   2017-11-16 20:06:10
+ * @Last Modified by:   lion1ou
+ * @Last Modified time: 2017-11-25 17:15:36
+ */
+const getDevice = require('./get/getDevice')
+
+const arraySort = require('./array/sort')
+const arrayUnquie = require('./array/unquie')
+
+const localItem = require('./localStorage/localItem')
+const rmLocalItem = require('./localStorage/removeLocalItem')
+
+const getCookie = require('./cookie/getCookie')
+const setCookie = require('./cookie/setCookie')
+
+const checkId = require('./check/checkId')
+const checkPhone = require('./check/checkPhone')
+const checkIsBottom = require('./check/checkIsBottom')
 
 module.exports = {
-  getDevice
+  getDevice,
+  arraySort,
+  arrayUnquie,
+  localItem,
+  rmLocalItem,
+  getCookie,
+  setCookie,
+  checkId,
+  checkPhone,
+  checkIsBottom
 }
